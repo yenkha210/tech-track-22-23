8f54b6ee9869cb9539d361e1db0adcf1fe2dc128
yenkha210 <90218478+yenkha210@users.noreply.github.com>
2022-10-31 15:58:49 +0100

    Codepen code

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,22 @@ import '../styles/style.css'
 import * as d3 from 'd3';
 
 console.log('Hello, world!');
+
+const data = [
+	1,
+	2,
+	"3",
+	"4",
+	5
+]
+
+function convertArrayStringsToNumbers() {
+	/* Your code here should convert the data array to an array containing only numbers and no strings and log the code to the console. */
+  let newData = data.map(item => {
+    return parseInt(item, 10);
+  })
+  
+  console.log(newData);
+}
+
+convertArrayStringsToNumbers();
