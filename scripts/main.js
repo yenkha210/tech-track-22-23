9ed2e7c9ccef13f08a4e8f04799790d825dcd6b4
yenkha210 <90218478+yenkha210@users.noreply.github.com>
2022-11-14 14:10:39 +0100

    vincent werken aan changenumber

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,45 @@
-// Our bundler automatically creates styling when imported in the main JS file!
 import '../styles/style.css'
-
-// We can use node_modules directely in the browser!
 import * as d3 from 'd3';
 
-console.log(d3)
+//import data
+/*
+const data = [
+	{
+		naam: "Pokemon",
+		release_year: "1999"
+
+	},
+	{
+		naam: "Pokemon",
+		release_year: "2005"
+
+	},
+	{
+		naam: "Pokemon",
+		release_year: "2012"
+
+	}
+
+]*/
 
 const data = new Array();
-
 d3.csv('../data/disney_plus_titles.csv', (line) => {
 	data.push(line);
+	//console.log(data);
+	changeNumber();
+	console.log(data);
 })
 
-console.log(data);
 
-// fetch('https://github.com/prasertcbs/basic-dataset/commit/b9b3ea98d027ff6f29c41cbc1a8a7db79a747b44')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+function changeNumber() {
+	data.map(item =>{
+	// return {
+	// 	release_year: parseInt(item.release_year, 10)
+	// }
+	return {[{]release_year_num: Number(item.release_year) }
+})
+}
+
+
+
 
