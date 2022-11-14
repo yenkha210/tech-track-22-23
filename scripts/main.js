16655f05a61166e16abd7640fff5906e278ac67e
yenkha210 <90218478+yenkha210@users.noreply.github.com>
2022-11-14 14:42:12 +0100

    Het begint ergens op te lijken met de string naar cijfer

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-import '../styles/style.css'
+import '../styles/style.css';
 import * as d3 from 'd3';
 
 //import data
+/*
 const data = [
 	{
 		naam: "Pokemon",
@@ -19,23 +20,27 @@ const data = [
 
 	}
 
-]
+]*/
 
-// new Array();
-// d3.csv('../data/disney_plus_titles.csv', (line) => {
-// 	data.push(line);
-// })
+const data = new Array();
+d3.csv('../data/disney_plus_titles.csv', (line) => {
+	data.push(line);
+	doeHet(data);
+})
 console.log(data);
 
 
-const changeNumber = data.map(item =>{
-	// return {
-	// 	release_year: parseInt(item.release_year, 10)
-	// }
-	return {release_year : Number(item.release_year) }
-})
 
 
-console.log(changeNumber);
+function doeHet(data) {
+	const changeNumber = data.map(item =>{ // Met .map kan je ieder item uit de array aanpassen
+		// return {
+		// 	release_year: parseInt(item.release_year, 10)
+		// }
+		return {release_year : Number(item.release_year) }
+	})
+	console.log(changeNumber);
+	// om dit allemaal te laten doen moet je doeHet(data) in de array hebben staan anders werkt het niet.
+}
 
 
