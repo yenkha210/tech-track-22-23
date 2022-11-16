38c62f21a64615aa6ee889bdc3770e4c5adc6b0f
yenkha210 <90218478+yenkha210@users.noreply.github.com>
2022-11-16 15:48:04 +0100

    Nadat Laura had geholpen om jaartallen te weergeven16 november

--- scripts/main.js
+++ scripts/main.js
@@ -22,20 +22,27 @@ const data = [
 
 ]*/
 
-const data = new Array();
-d3.csv('../data/disney_plus_titles.csv', (line) => {
-	data.push(line);
-	doeHet(data);
-})
-console.log(data);
+//const data = new Array();
+//d3.csv('../data/disney_plus_titles.csv', (line) => {
+	//data.push(line);
+	//doeHet(data);
+//})
+//console.log(data);
+
+let data = d3.json('../data/disneyplustitles.json')
+    .then(d => data = d)
+    .then(d => doeHet(d))
+    .catch(e => console.log("dataophalenmislukt"));
 
 
-
-
-function doeHet(data) {
-	const changeNumber = data.map(item =>{ // Met .map kan je ieder item uit de array aanpassen
+function actueleFilms(data) {
+	const dataFilter = data.map(item =>{ // Met .map kan je ieder item uit de array aanpassen
 		// return {
 		// 	release_year: parseInt(item.release_year, 10)
+		// return alle titels van films uit het jaar 2021
+		// if release_year > dan 2020, dan return een object met de titel van de film
+		// vertaal bovenste rij naar code!!!
+		//
 		// }
 		return {release_year : Number(item.release_year) }
 	})
